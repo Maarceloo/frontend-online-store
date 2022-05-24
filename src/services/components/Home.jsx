7b0f2273dfe8d98aa1bc90a84f209075ed66cccf
Samuelr499 <samuelribeiro2@hotmail.com>
2022-05-24 16:30:30 -0300

    Exercicio 02 Finalizado

---
+++ src/services/components/Home.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+        />
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </div>
+    );
+  }
+}
