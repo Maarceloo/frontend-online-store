24e5255a2cb3e0454b5a136fe8d6749b94b5ca8f
Lucas Abati Zanotto <lucasabatizanotto@gmail.com>
2022-05-24 19:18:49 -0300

    Requisito 04 finalizado

--- src/services/components/Home.jsx
+++ src/services/components/Home.jsx
@@ -1,22 +1,69 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../api';
 
 export default class Home extends Component {
-  render() {
-    return (
-      <div>
-        <nav>
-          <Link to="/shopping-cart" data-testid="shopping-cart-button">
-            <button type="button">Carrinho</button>
-          </Link>
-        </nav>
-        <input
-          type="text"
-        />
-        <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
-      </div>
-    );
+  state = {
+    categorieList: [],
   }
+
+  componentDidMount() {
+    this.getCategoriesApi();
+  }
+
+   getCategoriesApi = async () => {
+     const api = await getCategories();
+     console.log(api);
+     this.setState({
+       categorieList: api.map((categorie) => categorie),
+     });
+   }
+
+   handleClick = ({ target }) => {
+     const { checked, id } = target;
+     console.log(checked, id);
+     //  if (checked ===)
+   }
+
+   render() {
+     const { categorieList } = this.state;
+     return (
+       <div>
+         <nav>
+           <Link to="/shopping-cart" data-testid="shopping-cart-button">
+             <button type="button">Carrinho</button>
+           </Link>
+         </nav>
+         <input
+           type="text"
+         />
+         <div>
+           <ul>
+             {categorieList.map((categorie) => (
+               (
+                 <label
+                   data-testid="category"
+                   key={ categorie.id }
+                   htmlFor={ categorie.name }
+                 >
+                   <br />
+                   <input
+                     onClick={ this.handleClick }
+                     value={ categorie.id }
+                     name="category"
+                     type="radio"
+                     id={ categorie.name }
+                   />
+                   {categorie.name}
+                 </label>
+               )
+             ))}
+           </ul>
+         </div>
+         <p data-testid="home-initial-message">
+           Digite algum termo de pesquisa ou escolha uma categoria.
+         </p>
+       </div>
+     );
+   }
 }
