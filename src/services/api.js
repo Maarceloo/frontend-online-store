efdc16c1d4daa7e1ccbac15cd1c84a4534e2d492
Marcelo De Lima <delima_marcelo@outlook.com>
2022-05-24 15:41:24 -0300

    Exercicio 01 Finalizado

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 export async function getCategories() {
-  // Implemente aqui
+  const retornoApi = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const categories = retornoApi.json();
+  return categories;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const retorntApi = await fetch(`https://api.mercadolibre.com/sites/MLA/search?=${categoryId}&q=${query}`);
+  const categories = retorntApi.json();
+  return categories;
 }
