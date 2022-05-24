6253767b0897debd70508d6b20a5fc813416906b
Gabriel <gabriel.ferreira2002@hotmail.com>
2022-05-24 17:02:33 -0300

    requisito 3 finalizado

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './services/components/Home';
+import ShoppingCart from './services/components/ShoppingCart';
 
 class App extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class App extends React.Component {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ Home } />
+          <Route exact path="/shopping-cart" component={ ShoppingCart } />
         </Switch>
       </BrowserRouter>
     );
--- src/services/components/Home.jsx
+++ src/services/components/Home.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
+        <nav>
+          <Link to="/shopping-cart" data-testid="shopping-cart-button">
+            <button type="button">Carrinho</button>
+          </Link>
+        </nav>
         <input
           type="text"
         />
